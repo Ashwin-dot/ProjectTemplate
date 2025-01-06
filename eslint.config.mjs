9943d924dee116620abe931706a5394ab9f60656
Ashwin-dot <khatiwadaashwin5@gmail.com>
2025-01-06 12:57:54 +0545

    husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['dist', 'node_modules']
+    ignores: ['dist', 'node_modules'],
   },
   {
-    rules : {
-      "no-console" : "warn"
-    }
-  }
-];
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+]
