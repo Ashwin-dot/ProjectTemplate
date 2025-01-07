cb4f3348d58e58e7cae2a164c01a6c27fd95c5fd
Ashwin-dot <khatiwadaashwin5@gmail.com>
2025-01-07 12:57:42 +0545

    logger setup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { Config } from './config'
 import app from './app'
+import { Logger } from 'winston'
+import logger from './config/logger'
 
 const startServer = () => {
   const PORT = Config.PORT
 
   try {
     app.listen(PORT, () => {
-      console.log(`App listening on PORT  :` + PORT)
+      logger.info('Server Listening on port', PORT)
     })
   } catch (error) {
     console.error(error)
